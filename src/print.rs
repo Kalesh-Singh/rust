5b85a83d483cc4d66cce7d6359ca18074c4466ca
Kalesh-Singh <kaleshsingh96@gmail.com>
2020-12-06 01:09:11 -0500

    Positional Arguments

--- src/print.rs
+++ src/print.rs
@@ -4,4 +4,7 @@ pub fn run() {
 
     // Basic formatting
     println!("{} is from {}", "Kalesh", "Mars");
+
+    // Positional  Arguments
+    println!("{0} is from {1} and {0} likes to {2}", "Kalesh", "Mars", "code");
 }
